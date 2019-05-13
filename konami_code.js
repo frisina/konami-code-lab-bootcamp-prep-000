ea6c08b55e7d202e884a29fa051784badc557a21
Marco Frisina <marco@frisina.xyz>
2019-05-13 12:17:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,16 +11,25 @@ const codes = [
   "a"
 ];
 
-let index = 0;
-var main = document.body;
+const main = document.body;
 
 function init() {
-  // your code here
-  main.addEventListener('click', function(event) {
-  if (event.key === codes) {
-  alert('I was clicked!');
-} else {
-  console.log(event.key);
+  document.addEventListener('keydown', onKeyDownHandler)
 }
-});
-}
+
+function onKeyDownHandler(e) {
+
+  const key = e.key;
+
+     if (key === code[index]) {
+       index++;
+
+        if (index === code.length) {
+          alert("Konami kool!");
+
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+    };
